1b3ede96528aa367eb026a331cd0aed7970244a4
Alistair70 <63516216+Alistair70@users.noreply.github.com>
2024-01-30 12:16:02 -0500

    added a demo button to login for testing/demos

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
+document.getElementById("login_btn").addEventListener("click", function() {
     // Gets user inputted credentials from form
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
@@ -43,10 +41,61 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
                 const expirationTime = new Date(now.getTime() + 15 * 60 * 1000);
                 
                 // Create a cookie string
-                localStorage.setItem('expense_tracker_cookie_container', encoded_id);                             
-                //window.location.href = "https://expense-tracker-dash-board.netlify.app"; 
-                console.log('done')
+                document.cookie = `expense_tracker_cookie_container=${encoded_id}; expires=${expirationTime.toUTCString()}; path=/`;
+                window.location.href = "https://expense-tracker-dash-board.netlify.app";   
             } 
         })
     }
 });
+
+document.getElementById("demo_button").addEventListener("click", function() {
+
+    const username = 'Jane25'
+    const password = '12345'
+
+    fetch('/user_login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: username, password: password })
+    })
+    .then(response => response.json())
+    .then(data => 
+    {
+        encoded_id = data.encoded_id
+        console.log(encoded_id)
+        cookie_name = "expense_tracker_cookie_container"   
+        //If Request is successful user is redirected to dashboard page
+        if(data.message === "Invalid username or password") 
+        {
+            document.getElementById("output").innerHTML = "Incorrect Username/Password";
+        }  
+            // If credentials are not valid, message is displayed to user showing "Incorrect Username/Password"
+        else if(data.message === "Login successful")
+        {
+            const now = new Date();
+            const expirationTime = new Date(now.getTime() + 15 * 60 * 1000);
+            
+            // Create a cookie string
+            document.cookie = `expense_tracker_cookie_container=${encoded_id};domain=.expense-tracker-demo.site; expires=${expirationTime.toUTCString()}; path=/`;
+            window.location.href = "https://expense-tracker-dash-board.netlify.app";    
+        } 
+    })
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch and display elements from the server
+    cookieCheck(); 
+
+});
+
+function cookieCheck()
+{
+    cookie_name = "expense_tracker_cookie_container"
+    if(document.cookie.split(';').some((item) => item.trim().startsWith(`${cookie_name}=`)))
+    {
+        console.log("true")
+        window.location.href = "https://expense-tracker-dash-board.netlify.app";
+    }
+}
